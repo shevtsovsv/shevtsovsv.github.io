6b7350200bb0536a19703ad84f04f23e6fa4db33
shevtsovsv <36815732+shevtsovsv@users.noreply.github.com>
2020-05-09 10:30:25 +0300

    Delete cart.js

--- lending/assets/shopping-cart/cart.js
+++
@@ -1,2 +0,0 @@
-
-var cart=JSON.parse(localStorage.getItem("cart"))||null;if(cart){AMP.setState({cart:cart});var cartwrapper=document.querySelector(".shoping-cart-wrapper"),csclass=cartwrapper.className,counter=document.querySelector(".cart-counter");-1===csclass.search("show")&&(cartwrapper.className+=" show");counter.textContent=cart.items.length}else AMP.setState({cart:{items:[],id:1}});
